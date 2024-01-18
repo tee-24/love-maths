cb2b360fcb8b416bb9dd86ddd6a979ca502808bb
Towbie Idowu <towbieidowu@gmail.com>
2024-01-18 14:55:53 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,9 @@ function runGame(gameType){
     if (gameType === 'addition'){
         displayAdditionQuestion(num1, num2);
     }
+    else if (gameType === 'multiply'){
+        displayMultiplyQuestion(num1, num2);
+    }
     else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -64,7 +67,10 @@ function calculateCorrectAnswer(){
     let operator = document.getElementById('operator').innerText;
 
     if (operator === '+'){
-        return [operand1 + operand2, 'addition']
+        return [operand1 + operand2, 'addition'];
+    } 
+    else if (operator === 'x'){
+        return [operand1 * operand2, 'multiply'];
     } 
     else{
         alert(`Unimplemented operator ${operator}`);
@@ -99,6 +105,9 @@ function displaySubtractQuestion(){
     
 }
 
-function displayMultiplyQuestion(){
+function displayMultiplyQuestion(operand1, operand2){
     
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x"
 }
